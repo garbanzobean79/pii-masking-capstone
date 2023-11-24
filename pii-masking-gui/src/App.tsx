ac06f63f457f1a3ee1acbaa41f823c6265af7d0f
sophika781 <sophika171.g@gmail.com>
2023-11-23 21:26:08 -0500

    created gui react project

---
+++ pii-masking-gui/src/App.tsx
@@ -0,0 +1,8 @@
+import Next from "./components/Next";
+
+function App(){
+//  return <h1>Application Name</h1>
+    return <div><Next/></div>
+}
+
+export default App;
---
+++ pii-masking-gui/src/components/Next.tsx
@@ -0,0 +1,10 @@
+function Next(){
+    return (
+    <>
+        <h1>Application Name</h1>
+        <h2>Next</h2>
+    </>
+    );
+}
+
+export default Next;
