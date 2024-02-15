32d69124b7f39e8d2a0bd4d7c867f8b80572da6a
sophika781 <sophika171.g@gmail.com>
2024-02-15 13:17:33 -0500

    merge

---
+++ pii-masking-gui/src/App.tsx
@@ -0,0 +1,24 @@
+import Anonymity from "./components/Anonymity";
+import Start from "./components/Start";
+import NEROutput from "./components/NEROutput";
+import Signup from "./components/Signup";
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+function App(){
+
+    return( 
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Start/>}/>
+                    <Route path="/EntitySelect" element={<Anonymity/>}/>
+                    <Route path="/NEROutput" element = {<NEROutput/>}/>
+                    <Route path="/Signup" element = {<Signup/>}/>
+                </Routes>
+            </Router>
+        </>
+    );
+}
+
+export default App;
